672aee20ea2ba5bd6ac2e0895563c1cd63293145
yaschool <nikkita.kotik64@gmail.com>
2026-04-14 17:29:13 +0400

    added main_page.html, templates was updated

--- pages.py
+++ pages.py
@@ -18,11 +18,14 @@ def create_main_page(data: TextData) -> str:
                            autho_btn_text='Авторизация',  # TODO
                            change_lang_btn_text=data.get_another_lang(),
                            type = PagesType.main.value,
-                           tech_info=data.get_phrase('tech_info'))
+                           tech_info=data.get_phrase('tech_info'),
+                           btn_list=games_list,
+                           button_count=5,
+                           add_spot_btn_text=data.get_phrase('add_spot_button'))
 
 
 def create_add_spot_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -32,7 +35,7 @@ def create_add_spot_page(data: TextData) -> str:  # TODO: добавить фо�
 
 
 def create_game_info_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -42,7 +45,7 @@ def create_game_info_page(data: TextData) -> str:  # TODO: добавить фо
 
 
 def create_game_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -52,7 +55,7 @@ def create_game_page(data: TextData) -> str:  # TODO: добавить форм�
 
 
 def create_guess_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -62,7 +65,7 @@ def create_guess_page(data: TextData) -> str:  # TODO: добавить форм
 
 
 def create_learn_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -72,7 +75,7 @@ def create_learn_page(data: TextData) -> str:  # TODO: добавить форм
 
 
 def create_map_choice_to_learn_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -83,7 +86,7 @@ def create_map_choice_to_learn_page(data: TextData) -> str:  # TODO: добав�
 
 def create_map_choice_to_guess_page(data: TextData) -> str:  # TODO: добавить форму
     # TODO: добавить кнопку Random
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -93,7 +96,7 @@ def create_map_choice_to_guess_page(data: TextData) -> str:  # TODO: добав�
 
 
 def create_map_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -103,7 +106,7 @@ def create_map_page(data: TextData) -> str:  # TODO: добавить форму
 
 
 def create_moder_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
@@ -113,7 +116,7 @@ def create_moder_page(data: TextData) -> str:  # TODO: добавить форм
 
 
 def create_account_page(data: TextData) -> str:  # TODO: добавить форму
-    return render_template('main_page.html',
+    return render_template('base_template.html',
                            lang=data.get_lang(),
                            title=title,
                            autho_btn_text='Авторизация',
