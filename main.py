71b710e2bf0db0894872ca9c8a6db4c154ee5275
AltaMac <ivanfsar@gmail.com>
2026-04-26 22:12:53 +0400

    работает кнопка в шаблоне регистрации

--- main.py
+++ main.py
@@ -873,7 +873,7 @@ def reqister():
         user = User(
             name=form.name.data,
             email=form.email.data,
-            about=form.about.data
+
         )
         user.set_password(form.password.data)
         db_sess.add(user)
