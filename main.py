18fd4e4fae86aa42ff0a0744d702ab6c5a22452b
AltaMac <ivanfsar@gmail.com>
2026-04-26 22:15:08 +0400

    site now works with regist template

--- main.py
+++ main.py
@@ -878,7 +878,7 @@ def reqister():
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        return redirect('/regist/ru')
     return render_template('register.html', title='Регистрация', form=form)
 
 @app.route('/autho/ru', methods=['POST', 'GET'])
